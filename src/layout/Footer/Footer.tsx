c7b0e90e05988b854bed819000e040dd6fb6f36d
Diana Forostiana <dianaforost@gmail.com>
2023-10-11 15:28:03 +0200

    fix: footer to data.json

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -3,53 +3,56 @@ import GitHubIcon from "@/icons/GitHubIcon";
 import GmailIcon from "@/icons/GmailIcon";
 import LinkedInIcon from "@/icons/LindedInIcon";
 import TelegramIcon from "@/icons/TelegramIcon";
+import data from "../../data/data.json";
 
 const Footer = () => {
+  const { footer } = data;
+  const { title, footerList, socialTitle, d } = footer;
   return (
     <footer className="min-h-[50vh] z-20 relative bg-white">
       <Container className="flex flex-col gap-[15px] md:flex-row justify-around py-[40px] sm:py-[60px] md:py-[80px]">
-        <h2 className="visually-hidden">Footer</h2>
+        <h2 className="visually-hidden">{title}</h2>
+        {footerList.map((item, index) => (
+          <div key={index} className="flex flex-col gap-[10px]">
+            <h3 className="text-[24px]">{item.title}</h3>
+            <ul className="flex flex-col gap-[5px] sm:gap-[15px] md:gap-[20px]">
+              {item.number && (
+                <li>
+                  <h4>{item.number}</h4>
+                  <ul>
+                    {item.phone.map((phoneNumber, phoneIndex) => (
+                      <li key={phoneIndex}>
+                        <a
+                          href={`tel:${phoneNumber}`}
+                          className="hover:text-darkGray focus:text-darkGray hover:border-b-[1px] hover:border-darkGray hover:pb-[1px] focus:border-b-[1px] focus:border-darkGray focus:pb-[1px] transition duration-300"
+                        >
+                          {phoneNumber}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              )}
+              {item.email && (
+                <li>
+                  <h4>Email</h4>
+                  <ul>
+                    <li>
+                      <a
+                        href={`mailto:${item.emailto}`}
+                        className="hover:text-darkGray focus:text-darkGray hover:border-b-[1px] hover:border-darkGray hover:pb-[1px] focus:border-b-[1px] focus:border-white focus:pb-[1px] transition duration-300"
+                      >
+                        {item.emailto}
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              )}
+            </ul>
+          </div>
+        ))}
         <div className="flex flex-col gap-[10px]">
-          <h3 className="text-[24px]">Contacts</h3>
-          <ul className="flex flex-col gap-[5px] sm:gap-[15px] md:gap-[20px]">
-            <li>
-              <h4>Phone number</h4>
-              <ul>
-                <li>
-                  <a
-                    href="tel:+380975372174"
-                    className="hover:text-darkGray focus:text-darkGray hover:border-b-[1px] hover:border-darkGray hover:pb-[1px] focus:border-b-[1px] focus:border-darkGray focus:pb-[1px] transition duration-300"
-                  >
-                    +380 (97) 537 21 74
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="tel:+48797638432"
-                    className="hover:text-darkGray focus:text-darkGray hover:border-b-[1px] hover:border-darkGray hover:pb-[1px] focus:border-b-[1px] focus:border-darkGray focus:pb-[1px] transition duration-300"
-                  >
-                    +48 797638432
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <h4>Email</h4>
-              <ul>
-                <li>
-                  <a
-                    href="mailto:dianaforost@gmail.com"
-                    className="hover:text-darkGray focus:text-darkGray hover:border-b-[1px] hover:border-darkGray hover:pb-[1px] focus:border-b-[1px] focus:border-white focus:pb-[1px] transition duration-300"
-                  >
-                    dianaforost@gmail.com
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-        <div className="flex flex-col gap-[10px]">
-          <h3 className="text-[24px]">Social media</h3>
+          <h3 className="text-[24px]">{socialTitle}</h3>
           <ul className="flex gap-[5px] sm:gap-[15px] md:gap-[20px]">
             <li>
               <GitHubIcon
@@ -82,7 +85,7 @@ const Footer = () => {
           </ul>
         </div>
         <p className="text-[200px] text-lighterGray absolute bottom-[0] right-[0]">
-          D
+          {d}
         </p>
       </Container>
     </footer>
